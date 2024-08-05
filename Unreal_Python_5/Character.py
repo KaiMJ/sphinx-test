b591df67f4306aa65bab3f3db3cf61c25ea7be8a
KaiMJ <mjkim2964@gmail.com>
2024-08-05 14:50:03 -0700

    basic character

---
+++ Unreal_Python_5/Character.py
@@ -0,0 +1,16 @@
+class UECharacter:
+
+    def __init__(self):
+        """
+        Some doc
+        """
+        pass
+
+
+    def pick_object(self, object_id):
+        """
+        Pick up object.
+        """
+        pass
+
+    
