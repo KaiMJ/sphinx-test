492e2f784f6e525ffd2bd79b9ae58ca65e99dae2
Yan <yzhuan13@jh.edu>
2024-08-07 20:30:30 -0400

    character doc fix indentation

--- Unreal_Python_5/Character.py
+++ Unreal_Python_5/Character.py
@@ -2,7 +2,7 @@ from typing import Tuple
 
 
 class UECharacter:
-    def __init__(self, 
+    def __init__(self,
                  name: str,
                  location: Tuple[float, float, float],
                  rotation: float,
@@ -30,78 +30,77 @@ class UECharacter:
         cls._id_counter += 1
         return result
 
+    # Interact with pick-up
 
-    #Interact with pick-up
     def pick_object(self, object_id):
         """
         Pick up object.
         :param object_id: Target object's object id.
 
-        :return: Whether pick up the object successfully as a bool. 
+        :return: Whether pick up the object successfully as a bool.
         """
         pass
-    
+
     def drop_down(self):
         """
         Drop down the object character is holding
         Note: Now character can hold an object in right hand so we don't need to specify which hand
-        
-        :return: Whether drop down the object successfully as a bool. 
+
+        :return: Whether drop down the object successfully as a bool.
         """
         pass
 
-    #Interact with door
+    # Interact with door
     def pass_door(self, object_id):
         """
         Pass the door.
         :param object_id: Target door's object id.
 
-        :return: Whether pass the door successfully as a bool. 
+        :return: Whether pass the door successfully as a bool.
         """
-        pass  
+        pass
 
+    # Interact with vehicle
 
-    #Interact with vehicle
     def enter_vehicle(self, vehicle_id):
         """
         Enter the vehicle.
         Note: Currently, character can only enter the front two doors as driver.
 
         :param vehicle_id: Target vehicle's vehicle id.
-        
-        :return: Whether enter the vehicle successfully as a bool. 
+
+        :return: Whether enter the vehicle successfully as a bool.
         """
         pass
-    
+
     def exit_vehicle(self):
         """
         Exit the vehicle.
         Note: Character are allowed to exit the vehilce only if the car is fully stopped.
-        
-        :return: Whether exit the vehicle successfully as a bool. 
+
+        :return: Whether exit the vehicle successfully as a bool.
         """
         pass
 
     def open_back_trunk(self, vehicle_id):
-         """
+        """
         Open the back trunk of the vehicle.
 
         :param vehicle_id: Target vehicle's vehicle id.
-        
-        :return: Whether open the trunk successfully as a bool. 
+
+        :return: Whether open the trunk successfully as a bool.
         """
         pass
 
     def close_back_trunk(self, vehicle_id):
-         """
+        """
         close the back trunk of the vehicle.
 
         :param vehicle_id: Target vehicle's vehicle id.
-        
+
         :return: Whether close the trunk successfully as a bool. 
         """
         pass
-
 
     def drive_to_location(self, location):
         """
@@ -113,13 +112,13 @@ class UECharacter:
         pass
 
     # Character's local motion
-    def move_character_to_location(self, location)
+    def move_character_to_location(self, location):
         """
         Move the character to a specific location. 
 
         :param location: Target location as a tuple (x, y, z) or (x, y).
         Note: User doesn't have to specify the z location, engine will deal with that value.
-        
+
         :return: character's final location as tuple(x, y, z)
         """
         pass
@@ -129,11 +128,11 @@ class UECharacter:
         Move the character to a specific object. 
 
         :param object_id: Target object's object id.
-        
+
         :return: Character's final location as tuple(x, y, z)
         """
         pass
-    
+
     def move_character_along_direction(self, direction, step_size=150):
         """
         Move the character along direction with one step. 
@@ -142,11 +141,11 @@ class UECharacter:
         :param direction: 4 directions are supplied. "forward" , "backward", "left" and "right" 
         represents the forward, backward, left and right direction of the character.
         :param step_size: Step size of this step. In default, it's set as 150.
-        
+
         :return: Character's final location as tuple(x, y, z)
         """
         pass
-    
+
     def move_character_towards_location(self, location, step_size=150):
         """
         Move the character towards a specific location with one step. 
@@ -155,11 +154,11 @@ class UECharacter:
 
         :param location: Target location as tuple (x, y, z) or (x, y).
         :param step_size: Step size of this step. In default, it's set as 150.
-        
+
         :return: Character's final location as tuple(x, y, z)
         """
         pass
-    
+
     def move_character_towards_object(self, object_id, step_size=150):
         """
         Move the character towards a specific object with one step. 
@@ -168,8 +167,7 @@ class UECharacter:
 
         :param object_id: Target object's object id.
         :param step_size: Step size of this step. In default, it's set as 150.
-        
+
         :return: Character's final location as tuple(x, y, z)
         """
         pass
-    
--- Unreal_Python_5/__init__.py
+++ Unreal_Python_5/__init__.py
@@ -2,6 +2,7 @@ from .Camera import UECamera
 from .Object import UEObject
 from .Engine import UE5_Engine
 from .Lighting import UELighting
+from .Character import UECharacter
 from .SceneGraph import SceneGraph
 
-__all__ = ['UECamera', 'UEObject', 'UE5_Engine', 'UELighting', 'SceneGraph']
+__all__ = ['UECamera', 'UEObject', 'UE5_Engine', 'UELighting', 'UECharacter', 'SceneGraph']
